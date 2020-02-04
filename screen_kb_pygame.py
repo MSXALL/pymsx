59488e0357968795e9f5d33195b6966d75157c72
Folkert van Heusden <mail@vanheusden.com>
2020-02-04 19:44:05 +0100

    wip

--- screen_kb_pygame.py
+++ screen_kb_pygame.py
@@ -26,8 +26,8 @@ class screen_kb_pygame(screen_kb):
         # TMS9918 palette 
         self.rgb = [ self.rgb_to_i(0, 0, 0), self.rgb_to_i(0, 0, 0), self.rgb_to_i(33, 200, 66), self.rgb_to_i(94, 220, 120), self.rgb_to_i(84, 85, 237), self.rgb_to_i(125, 118, 252), self.rgb_to_i(212, 82, 77), self.rgb_to_i(66, 235, 245), self.rgb_to_i(252, 85, 84), self.rgb_to_i(255, 121, 120), self.rgb_to_i(212, 193, 84), self.rgb_to_i(231, 206, 128), self.rgb_to_i(33, 176, 59), self.rgb_to_i(201, 91, 186), self.rgb_to_i(204, 204, 204), self.rgb_to_i(255, 255, 255) ] 
 
-        self.screen = pygame.display.set_mode((256, 192))
-        self.surface = pygame.Surface((256, 192))
+        self.screen = pygame.display.set_mode((320, 192))
+        self.surface = pygame.Surface((320, 192))
         self.arr = pygame.surfarray.array2d(self.screen)
 
     def stop2(self):
@@ -66,6 +66,10 @@ class screen_kb_pygame(screen_kb):
 
             msg = self.debug_msg[0:79]
 
+            s = time.time()
+            hit = 0
+            hitdiv = 1
+
             # redraw
             vm = self.video_mode()
             if vm == 1:  # 'screen 2' (256 x 192)
@@ -73,15 +77,14 @@ class screen_kb_pygame(screen_kb):
                 bg_colors = (self.registers[3] & 128) <<  6
                 bg_tiles  = (self.registers[4] &   4) << 11
 
-                s = time.time()
-
                 par = pygame.PixelArray(self.surface)
 
                 pb = None
                 cache = None
 
-                hit = 0
                 tiles_offset = colors_offset = 0
+
+                hitdiv = 32 * 24
 
                 for map_index in range(0, 32 * 24):
                     block_nr    = (map_index >> 8) & 3
@@ -127,10 +130,56 @@ class screen_kb_pygame(screen_kb):
                 pygame.surfarray.blit_array(self.screen, self.arr)
                 pygame.display.flip()
 
-                print('daar %f %f | %.2f' % (s, time.time() - s, hit * 100.0 / (32 * 24)))
+            elif vm == 4:  # 40 x 24
+                cols = 40  # FIXME
+                hitdiv = cols * 24
+
+                bg_map = (self.registers[2] & 0x7c) << 10 if cols == 80 else (self.registers[2] & 15) << 10
+                bg_tiles = (self.registers[4] & 7) << 11
+
+                bg = self.rgb[self.registers[7] & 15]
+                fg = self.rgb[self.registers[7] >> 4]
+
+                par = pygame.PixelArray(self.surface)
+
+                pb = None
+                cache = [ None ] * 256
+
+                tiles_offset = colors_offset = 0
+
+                for map_index in range(0, 40 * 24):
+                    cur_char_nr = self.ram[bg_map + map_index]
+
+                    scr_x = (map_index % cols) * 8;
+                    scr_y = (map_index // cols) * 8;
+
+                    if cache[cur_char_nr] == None:
+                        cache[cur_char_nr] = [ 0 ] * 64
+
+                        cur_tiles = bg_tiles + cur_char_nr * 8
+
+                        for y in range(0, 8):
+                            current_tile = self.ram[cur_tiles]
+                            cur_tiles += 1
+
+                            for x in range(0, 8):
+                                cache[cur_char_nr][y * 8 + x] = fg if (current_tile & 128) == 128 else bg
+                                current_tile <<= 1
+                    else:
+                        hit += 1
+
+                    for y in range(0, 8):
+                        for x in range(0, 8):
+                            self.arr[scr_x + x, scr_y + y] = cache[cur_char_nr][y * 8 + x]
+
+                pygame.surfarray.blit_array(self.screen, self.arr)
+                pygame.display.flip()
 
             else:
                 msg = 'Unsupported resolution'
+
+            took = time.time() - s
+            print('fps: %f, cache hit: %.2f%%' % (1 / took, hit * 100.0 / hitdiv))
 
             self.debug_msg_lock.acquire()
             if self.debug_msg:
