952723aeb65288b5fff115adf3610d424bbcd5a0
Folkert van Heusden <mail@vanheusden.com>
2020-02-11 10:09:25 +0100

    scc pages wrap

--- scc.py
+++ scc.py
@@ -27,9 +27,9 @@ class scc:
         p = self.scc_pages[bank] * 0x2000 + offset
 
         if offset == 0x1000: # 0x5000, 0x7000 and so on
-            self.debug('Set bank %d to %d (%04x)' % (bank, v, a))
-            assert v < self.n_pages
-            self.scc_pages[bank] = v
+            and_ = v & (self.n_pages - 1)
+            self.debug('Set bank %d to %d/%d (%04x)' % (bank, v, and_, a))
+            self.scc_pages[bank] = and_
 
         else:
             self.debug('SCC write to %04x not understood' % a)
